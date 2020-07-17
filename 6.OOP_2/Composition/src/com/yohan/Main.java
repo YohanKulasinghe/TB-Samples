244a73ec5163e533dd695f7103a9de52ed1a16f6
yohan kulasinghe <yohankulasinghe@gmail.com>
2020-07-17 23:13:30 +0530

    add oop2 composition 2

--- 6.OOP_2/Composition/src/com/yohan/Main.java
+++ 6.OOP_2/Composition/src/com/yohan/Main.java
@@ -2,12 +2,12 @@ package com.yohan;
 
 public class Main {
 
-    public static void Main (String[] args){
+    public static void main (String[] args){
         Door door1 = new Door(123,456,5);
         Table table1 = new Table(4, new TableSurface(50,100),new Drawer(5));
 
         Room room = new Room(door1,table1);
 
-        room.getDoor();
+        room.getDoor().openDoor();
     }
 }
